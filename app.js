038017eeb87f838965db452595e89061888ae745
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-11-14 15:47:55 +0530

    final

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const index = require('./routes/index');
 const auth = require('./routes/auth');
 const uploadContainer = require('./'); 
 const cors = require('cors')
+
+var multer  = require('multer');
+var fs  = require('fs');
+
 require("dotenv").config();
 
 
@@ -22,6 +26,28 @@ app.use('/', index);
 
 // 
 // app.post("/multiple-upload", uploadContainer.multer);
+
+// var storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         var dir = './uploads';
+//         if (!fs.existsSync(dir)){
+//             fs.mkdirSync(dir);
+//         }
+//         callback(null, dir);
+//     },
+//     filename: function (req, file, callback) {
+//         callback(null, file.originalname);
+//     }
+// });
+// var upload = multer({storage: storage}).array('files', 12);
+// app.post('/upload', function (req, res, next) {
+//     upload(req, res, function (err) {
+//         if (err) {
+//             return res.end("Something went wrong:(");
+//         }
+//         res.end("Upload completed.");
+//     });
+// })
 
 
 //Import Route 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const hirexp = require('express').Router()
 const mongoose = require('mongoose')
 const path = require("path")
 const crypto = require("crypto")
-const multer = require("multer")
+var multer  = require('multer');
+var fs  = require('fs');
 const uploadContainer = require('./'); 
 const gridFs = require("multer-gridfs-storage")
 const gridFstream = require("gridfs-stream");
@@ -120,6 +121,30 @@ hirexp.get('/explore', async (req,res)=>{
     
 })
 
+
+// var storage = multer.diskStorage({
+//   destination: function (req, file, callback) {
+//       var dir = './uploads';
+//       if (!fs.existsSync(dir)){
+//           fs.mkdirSync(dir);
+//       }
+//       callback(null, dir);
+//   },
+//   filename: function (req, file, callback) {
+//       callback(null, file.originalname);
+//   }
+ 
+// });
+// var upload = multer({storage: storage}).array('files', 12);
+// hirexp.post('/upload', function (req, res, next) {
+//   upload(req, res, function (err) {
+//       if (err) {
+//           return res.end("Something went wrong:(");
+//       }
+//       res.end("Upload completed.");
+//       console.log("Uploaded");
+//   });
+// })
   
 // var storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
