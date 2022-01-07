dc92d0a277788c4957f014961f0623dd2b7db0e0
madhhuurrii <madhuriramakrishnan061@gmail.com>
2022-01-07 18:14:30 +0530

    add

--- app.js
+++ app.js
@@ -24,30 +24,6 @@ app.use('/', index);
 
 
 
-// 
-// app.post("/multiple-upload", uploadContainer.multer);
-
-// var storage = multer.diskStorage({
-//     destination: function (req, file, callback) {
-//         var dir = './uploads';
-//         if (!fs.existsSync(dir)){
-//             fs.mkdirSync(dir);
-//         }
-//         callback(null, dir);
-//     },
-//     filename: function (req, file, callback) {
-//         callback(null, file.originalname);
-//     }
-// });
-// var upload = multer({storage: storage}).array('files', 12);
-// app.post('/upload', function (req, res, next) {
-//     upload(req, res, function (err) {
-//         if (err) {
-//             return res.end("Something went wrong:(");
-//         }
-//         res.end("Upload completed.");
-//     });
-// })
 
 
 //Import Route 
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const hirexp = require('express').Router()
 const mongoose = require('mongoose')
-const path = require("path")
-const crypto = require("crypto")
-var multer  = require('multer');
-var fs  = require('fs');
-const uploadContainer = require('./'); 
-const gridFs = require("multer-gridfs-storage")
-const gridFstream = require("gridfs-stream");
-const metod = require("method-override")
-const util = require('util');
 const Explore = require('../model/explore');
 
 // const path = require("path")
@@ -69,12 +60,7 @@ hirexp.post('/hirenow',  async (req, res) => {
   } = req.body
 
  
-  // const obj= {
-  //   img:{
-  //     data: fs.readFileSync(path.join(__dirname+"/public"+req.file.filename)),
-  //     contentType: 'image/png'
-  //   }
-  // }
+  
   
 
 
@@ -95,11 +81,7 @@ hirexp.post('/hirenow',  async (req, res) => {
     })
     console.log('Job Openeing created Successfully!', data1)
   
-    // const data2 = new Explore({
-    //     avatar: result.secure_url,
-    //     cloudinary_id: result.public_id,
-    // });
-    // await data2.save();
+   
     
   } catch (error) {
     if (error.code === 11000) {
@@ -122,79 +104,4 @@ hirexp.get('/explore', async (req,res)=>{
 })
 
 
-// var storage = multer.diskStorage({
-//   destination: function (req, file, callback) {
-//       var dir = './uploads';
-//       if (!fs.existsSync(dir)){
-//           fs.mkdirSync(dir);
-//       }
-//       callback(null, dir);
-//   },
-//   filename: function (req, file, callback) {
-//       callback(null, file.originalname);
-//   }
- 
-// });
-// var upload = multer({storage: storage}).array('files', 12);
-// hirexp.post('/upload', function (req, res, next) {
-//   upload(req, res, function (err) {
-//       if (err) {
-//           return res.end("Something went wrong:(");
-//       }
-//       res.end("Upload completed.");
-//       console.log("Uploaded");
-//   });
-// })
-  
-// var storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'images')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-  
-// var upload = multer({ storage: storage });
-
-// hirexp.get("/show",(req,res)=>{
-//   Explore.find().toArray(function (err,result){
-//      const imgArray = result.map(element =>element._id);
-//      console.log(imgArray);
-//      if(err){
-//          return console.error(err);
-//      }
-//      res.send(imgArray)
-//  })
-// });
-// hirexp.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
-//   var img = fs.readFileSync(req.file.path);
-//   var encode_img = img.toString('base64');
-//   var final_img = {
-//       contentType:req.file.mimetype,
-//       image:new Buffer(encode_img,'base64')
-//   };
-//   Explore.create(final_img,function(err,result){
-//       if(err){
-//           console.log(err);
-//       }else{
-//           console.log(result.img.Buffer);
-//           console.log("Saved To database");
-//           res.contentType(final_img.contentType);
-//           res.send(final_img.image);
-//       }
-//   })
-// })
-
-// app.get('/', (req, res) => {
-//   Explore.find({}, (err, items) => {
-//       if (err) {
-//           console.log(err);
-//           res.status(500).send('An error occurred', err);
-//       }
-//       else {
-//           res.render('imagesPage', { items: items });
-//       }
-//   });
-// });
 module.exports = hirexp;
